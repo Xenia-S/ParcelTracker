cff4f23ba40c0b3ae784d4da7a3e6155da337f51
Xenia <thebuginbox@gmail.com>
2023-11-30 22:37:37 +0600

    #1

---
+++ src/test/java/com/example/parseltracker/ParselTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parseltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParselTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
