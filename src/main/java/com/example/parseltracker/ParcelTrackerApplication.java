b9896018f09f6875145c5d804c9384b6518fcb4f
Xenia <thebuginbox@gmail.com>
2023-11-30 22:37:37 +0600

    #1

---
+++ src/main/java/com/example/parseltracker/ParcelTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.example.parseltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcelTrackerApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
